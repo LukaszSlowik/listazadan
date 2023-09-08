76e99937f6411fc28acf32775685c8a3f2de7cac
Lukasz <lukasz.slowik.osw@gmail.com>
2023-09-08 15:19:33 +0200

    Copy changes from main branch to new-branch

--- src/lib/fetchFunctions/fetchFunctions.ts
+++ src/lib/fetchFunctions/fetchFunctions.ts
@@ -2,7 +2,15 @@ import { Task } from "../validations/Task";
 
 export async function getTasks(): Promise<Task[]> {
   try {
-    const res = await fetch("/api/getTasks", { cache: "no-store" });
+    const res = await fetch(
+      "/api/getTasks", //{ cache: "no-store" }
+      {
+        next: {
+          revalidate: 1,
+        },
+      },
+    );
+
     const tasks = await res.json();
 
     console.log("will return tasks", tasks);
